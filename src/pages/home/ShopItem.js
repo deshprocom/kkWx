27a607a24e6007a7851936b5bb2d85d205d63a06
lornem <ailuo8899@gmail.com>
2018-12-14 15:58:19 +0800

    倒计时状态

--- src/pages/home/ShopItem.js
+++ src/pages/home/ShopItem.js
@@ -1,7 +1,9 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View, Swiper, SwiperItem, Image, ScrollView } from '@tarojs/components';
 import './index.scss';
-const baseUrl = 'https://cdn-upyun.deshpro.com/kk/uploads/'
+import { AtCountDown } from 'taro-ui'
+import { logMsg, dateFormat } from '../../utils/utils';
+
 
 export default class ShopItem extends Component {
 
@@ -9,12 +11,52 @@ export default class ShopItem extends Component {
     Taro.navigateTo(e.currentTarget.dataset)
   }
 
+  dayHour = (millis) => {
+    let minute = 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+    if(millis<0)
+     return
+
+    let dayC = millis/day
+    let hourC = millis/hour
+    let minC = millis/minute
+    
+    logMsg(`${dayC} 天 ${hour} 时 ${minC} 分`)
+  }
+
   render() {
-    const { icon, title, original_price, price, product_id, returnable, saleable_num, sales_volume } = this.props.item;
+    let curDate = new Date()
+    let curTimes = Math.round(curDate.getTime() / 1000)
+
+    const { icon, title, original_price, price,
+      product_id, returnable, saleable_num, sales_volume,
+      end_time, begin_time } = this.props.item;
+      logMsg(`begin_time ${dateFormat(begin_time)}`)
+      logMsg(`end_time ${dateFormat(end_time)}`)
+    let diff = curTimes - begin_time
+    logMsg("当前时间", diff)
+    let canBuyStr = "距离结束"
+    if(diff>0){
+      canBuyStr = `距离开始`
+    }else{
+      diff = curTimes - end_time
+      if(diff>0){
+        canBuyStr = `已结束`
+      }else{
+        diff = Math.abs(diff)
+      }
+    }
+
     return (<View className="item" data-url="/pages/ShopDetail/index" onClick={this.goDetailPage}>
       <View className="content">
         <View className="tag_count_down">
-          <Text className="txt">10:35:09</Text>
+          <Text style={"color:#fff;"}>{canBuyStr}</Text>
+          <AtCountDown 
+          style={"color:#fff;"}
+          isShowDay
+          format={{ day: '天',hours: '时',minutes: '分',seconds: '秒' }}
+          seconds={diff}/>
         </View>
 
         {/* <View className="tag_shop">
