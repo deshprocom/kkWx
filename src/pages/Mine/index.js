e7b629ae48b8d83e602a30da600a1812a58c5e28
lorne <2270333671@qq.com>
2018-12-18 17:00:50 +0800

    我的 页面

--- src/pages/Mine/index.js
+++ src/pages/Mine/index.js
@@ -115,7 +115,7 @@ export default class Mine extends Component {
                 <Text className="price_text">¥39.9</Text>
                 <Text className="origin_price_text">¥69.9</Text>
                 <View style='display:flex;flex:1'/>
-                <Text className="count_text">X1</Text>
+                <Text className="count_text"><Text className="count_text1">X</Text>1</Text>
               </View>
               <View className="right_view_middle">
                 <View className="use_btn_view">
