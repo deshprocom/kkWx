5f0550b2bd123cf956878627159e63866ccefbad
lorne <2270333671@qq.com>
2018-12-21 12:16:01 +0800

    订单详情页面

--- src/pages/OrderDetail/index.js
+++ src/pages/OrderDetail/index.js
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
 import './index.scss';
+import OrderItem from '../../components/order/OrderItem'
 
 @connect(({OrderDetail}) => ({
   ...OrderDetail,
@@ -18,7 +19,31 @@ export default class Orderdetail extends Component {
   render() {
     return (
       <View className="OrderDetail-page">
-        OrderDetail
+        <View className="detail_top_view">
+          <Text className="top_text">商品信息</Text>
+        </View>
+        <OrderItem/>
+
+        <View className="detail_top_view" style="margin-top:10px;">
+          <Text className="top_text">订单信息</Text>
+        </View>
+        <View className="detail_list_view">
+          <Text className="detail_text1">订单编号：20180907367</Text>
+          <Text className="detail_text1">下单时间：2018年12月3日 14:30</Text>
+          <Text className="detail_text1">商品金额：¥39</Text>
+          <Text className="detail_text1">实际付款：¥38</Text>
+          <View className="last_detail_view">
+            <Text className="detail_text3">使用状态：</Text>
+            <Text className="detail_text2">待使用</Text>
+          </View>
+        </View>
+        <View className="erweima_view">
+          <Text className="text1">商家扫码</Text>
+        </View>
+
+        <View className="btn_view">
+          <Text className="top_text">联系客服</Text>
+        </View>
       </View>
     )
   }
