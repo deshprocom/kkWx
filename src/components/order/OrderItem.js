33015c6c59cb57a16c078ce74e1baff628ed50a2
lorne <2270333671@qq.com>
2018-12-25 17:59:48 +0800

    订单列表item接口数据

--- src/components/order/OrderItem.js
+++ src/components/order/OrderItem.js
@@ -12,8 +12,17 @@ export default class OrderItem extends Component {
   }
 
   render() {
-    const {item} = this.props
-    let product = item.order_items[0]
+    const {item} = this.props;
+    const {created_at,final_price,order_items,order_number,pay_status,refunded_price,shipping_price,
+      status,total_price,total_product_price} = item;
+      
+    let product = item.order_items[0];
+    const {image,number,original_price,price,product_id,return_status_text,returnable} = product;
+    
+    let status_text = return_status_text === "" ? <View/> :<View className="use_btn_view">
+    <Text className="use_text">{return_status_text}</Text>
+  </View>
+
     return (<View className="order_list_view"
       onClick={this.goOrderDetail.bind(this,item)}>
       <View className="list_view">
@@ -24,16 +33,14 @@ export default class OrderItem extends Component {
         <View className="list_view_right">
           <Text className="intro_text">{product.title}</Text>
           <View className="right_view_middle">
-            <Text className="price_text">{`¥${23}`}</Text>
-            <Text className="origin_price_text">{`¥${46}`}</Text>
+            <Text className="price_text">{`¥${product.price}`}</Text>
+            <Text className="origin_price_text">{`¥${product.original_price}`}</Text>
             <View style='display:flex;flex:1' />
-            <Text className="count_text"><Text className="count_text1">X</Text>1</Text>
+            <Text className="count_text"><Text className="count_text1">X</Text>{product.number}</Text>
           </View>
           <View className="right_view_middle">
-            <View className="use_btn_view">
-              <Text className="use_text">待使用</Text>
-            </View>
-            <Text className="freight_text">运费：¥39</Text>
+            {status_text}
+            <Text className="freight_text">运费：¥{refunded_price}</Text>
           </View>
         </View>
       </View>
