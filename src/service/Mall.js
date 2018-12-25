4e35c836118da5ed566e9b5c34e3aac34260eeda
lornem <ailuo8899@gmail.com>
2018-12-25 16:31:13 +0800

    微信支付

--- src/service/Mall.js
+++ src/service/Mall.js
@@ -5,14 +5,18 @@
  */
 import { get, post } from '../utils/request'
 import api from '../config/api'
-
+import Taro from '@tarojs/taro';
+import { logMsg, showToast } from '../utils/utils';
 
 export function getShopCategorios(resolve, reject) {
   get(api.shopCategories, {}, resolve, reject)
 }
 
 export function createOrder(param, resolve, reject) {
-  post(api.shop_order, param, resolve, reject)
+  post(api.shop_order, param, ret=>{
+    resolve(ret)
+    shopWxPay(ret.order_number)
+  }, reject)
 
 }
 
@@ -26,4 +30,32 @@ export function shopOrderList(param, resolve, reject) {
 
 export function shopOrderDetail(id,resolve,reject){
   get(`${api.shop_order}/${id}`, {}, resolve, reject)
+}
+
+export function shopWxPay(order_num,resolve,reject){
+  post(`${api.shop_order}/${order_num}/wx_pay`, {trade_source:'miniprogram'}, ret=>{
+    let pay = {}
+    pay.timeStamp = ret.timestamp
+    pay.nonceStr = ret.noncestr
+    pay.signType = 'MD5'
+    pay.paySign = ret.sign
+    pay.package = ret.package
+    let callback = {success:(res)=>{
+      logMsg('微信支付结果1',res)
+      shopWxPayEnd(order_num,result=>{
+        logMsg('微信支付结果2',result)
+      })
+    },fail:(res)=>{
+      showToast(res.err_desc)
+      logMsg('微信支付失败',res)
+    }}
+    Object.assign(pay,callback)
+    logMsg('微信支付参数',pay)
+    Taro.requestPayment(pay)
+
+  }, reject)
+}
+
+export function shopWxPayEnd(order_num,resolve,reject){
+  post(`${api.shop_order}/${order_num}/wx_paid_result`, param, resolve, reject)
 }
